a758862066f5ceaac3d19e2c016448beb04f3f72
zhampel <{ID}+{username}@users.noreply.github.com>
2020-04-04 19:28:34 -0700

    Cleans up mounting with updated lookups to .ssh/config. One lines the unmounting process

--- bash/mnt_machines.sh
+++ bash/mnt_machines.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-global_mounting_dir="${HOME}/MNTMachines"
+global_mounting_dir="$1"
+#global_mounting_dir="${HOME}/MNTMachines"
 
 # Lookup hostname IP of named machine
 function hname_lookup {
@@ -14,52 +15,69 @@ function username_lookup {
   echo ${username}
 }
 
+# Lookup username of named machine
+function idfile_lookup {
+  local idfile=`ssh -G ${1} | awk '/^identityfile / { print $2 }'`
+  echo ${idfile}
+}
+
 # Mount requested machine
 function mymnt {
-  echo "Mounting $1 machine..."
+  machine=${1}
+  echo "Mounting ${machine} machine..."
 
   # Mounting directory
-  m_dir=${global_mounting_dir}/$1
+  m_dir=${global_mounting_dir}/${machine}
 
   # If mounting directory dne, make it
-  if [ ! -d ${m_dir} ]; then
-    mkdir ${m_dir}
-  fi
+  [ ! -d ${m_dir} ] && mkdir ${m_dir}
 
   # Get hostname address of machine
-  hname=$(hname_lookup $1)
+  hname=$(hname_lookup ${machine})
   echo "  with HOSTNAME:  ${hname}"
 
   # Get hostname address of machine
-  usrname=$(username_lookup $1)
+  usrname=$(username_lookup ${machine})
   echo "  with USERNAME:  ${usrname}"
 
   # RSA identify file
-  rsa_file="~/.ssh/id_rsa_${1}"
+  rsa_file=$(idfile_lookup ${machine})
+  # Check that only one IdentityFile is listed, no default.
+  #[ ${#rsa_file} -eq 1 ] || { echo "IdentityFile may not be properly defined in .ssh/config."; return 0; }
+  echo "  with IDENTITYFILE:  ${rsa_file}"
 
+  #echo "sudo sshfs -o allow_other,defer_permissions,IdentityFile=${rsa_file} ${usrname}@${hname}: ${m_dir}"
+  #echo "sshfs -o allow_other,defer_permissions,IdentityFile=${rsa_file} ${usrname}@${1}: ${m_dir}"
   # SSHFS command to mount
   if [ -f ${rsa_file} ]; then
-    sudo sshfs -o allow_other,defer_permissions,IdentityFile=${rsa_file} ${usrname}@${hname}: ${m_dir}
+    eval `sshfs -o allow_other,defer_permissions,IdentityFile=${rsa_file} ${usrname}@${machine}: ${m_dir}`
+    #sshfs -o allow_other,defer_permissions,IdentityFile=${rsa_file} ${usrname}@${1}: ${m_dir}
+    #sshfs -o allow_other,defer_permissions,IdentityFile=${rsa_file} ${usrname}@${hname}: ${m_dir}
+    #sudo sshfs -o allow_other,defer_permissions,IdentityFile=${rsa_file} ${usrname}@${hname}: ${m_dir}
     echo "Successfully mounted to ${m_dir}"
   else
-    sudo sshfs -o allow_other,defer_permissions ${usrname}@${hname}: ${m_dir}
+    #sshfs -o allow_other,defer_permissions ${usrname}@${hname}: ${m_dir}
+    #sudo sshfs -o allow_other,defer_permissions ${usrname}@${hname}: ${m_dir}
     echo "Successfully mounted to ${m_dir}"
   fi
 }
 
 # Unmount requested machine
 function unmntme {
-  echo "Unmounting $1 machine..."
+  machine=${1}
+  echo "Unmounting ${machine} machine..."
 
   # Mounting directory
-  m_dir=${global_mounting_dir}/$1
+  m_dir=${global_mounting_dir}/${machine}
   
   # If mounting directory dne, exit
-  if [ ! -d ${m_dir} ]; then
-    echo "Mounted directory does not exist. Exiting..."
-  else
-    sudo diskutil unmount force ${m_dir}
-    #sudo umount -f ${m_dir}
-    echo "Successfully unmounted."
-  fi
+  [ ! -d ${m_dir} ] && echo "Mounted directory does not exist. Exiting..." || \
+  sudo diskutil unmount force ${m_dir} && echo "Successfully unmounted."
+  #if [ ! -d ${m_dir} ]; then 
+  #  echo "Mounted directory does not exist. Exiting..."
+  #else
+  #  sudo diskutil unmount force ${m_dir}
+  #  #sudo umount -f ${m_dir}
+  #  echo "Successfully unmounted."
+  #fi
 }
