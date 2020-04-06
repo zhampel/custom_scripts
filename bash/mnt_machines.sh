aa8698b6413017ee9bf65d81f4d2a6dc13fb0aba
zhampel <{ID}+{username}@users.noreply.github.com>
2020-04-06 15:56:44 -0700

    Cleanup of print statements

--- bash/mnt_machines.sh
+++ bash/mnt_machines.sh
@@ -29,20 +29,20 @@ function mymnt {
   is_mnt=`mount | grep "on ${m_dir}"`
   # If not then mount, otherwise return from function.
   { [ -z "${is_mnt}" ] && echo "Mounting ${machine} to directory ${m_dir}"; } || \
-  { echo "Machine ${machine} already mounted to ${m_dir}. Exiting..." && return 0; }
+  { echo -e "\tMachine ${machine} already mounted to ${m_dir}. Exiting..." && return 0; }
 
   # Get hostname address of machine
   hoststring="hostname"
   hname=$(sshconfig_lookup ${machine} ${hoststring})
   ping -c 1 ${hname} &> /dev/null || \
-  { echo "Machine ${machine} with hostname ${hname} not reachable as defined. Exiting..." && return 0; }
-  echo "  with HOSTNAME:  ${hname}"
+  { echo -e "\tMachine ${machine} with hostname ${hname} not reachable as defined. Exiting..." && return 0; }
+  echo -e "\twith HOSTNAME:  ${hname}"
 
   # Get username associated with machine, defaults to local user
   userstring="user"
   usrname=$(sshconfig_lookup ${machine} ${userstring})
   [ -z ${usrname} ] && usrname=`whoami`
-  echo "  with USERNAME:  ${usrname}"
+  echo -e "\twith USERNAME:  ${usrname}"
 
   # Get RSA identify file
   identitystring="identityfile"
@@ -53,12 +53,12 @@ function mymnt {
   arr=($rsa_file)
   [ ${#arr[@]} -eq 1 ] || \
   { echo "IdentityFile for ${machine} machine not specified in ~/.ssh/config."; \
-  echo "    Please specify or consider from amongst default options:"; \
-  echo "    ${rsa_file}"; return 0; }
+  echo -e "\tPlease specify or consider from amongst default options:"; \
+  echo -e "\t${rsa_file}"; return 0; }
 
   # Test that specified RSA file exists
   [ -f ${rsa_file} ] || { echo "IdentifyFile ${rsa_file} does not exist. Exiting..."; return 0; }
-  echo "  with IDENTITYFILE:  ${rsa_file}"
+  echo -e "\twith IDENTITYFILE:  ${rsa_file}"
 
   eval `sshfs -o allow_other,defer_permissions,IdentityFile=${rsa_file} ${usrname}@${machine}: ${m_dir}` && \
   echo "Successfully mounted ${machine} to ${m_dir}" || \
